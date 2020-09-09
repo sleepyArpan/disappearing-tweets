a3caec5dc79fa10135e48ac7e919b5e1630e987a
sleepyArpan <arpanchattopadhyay23@gmail.com>
2020-09-09 12:41:08 +0530

    added text area and button

--- src/components/Tweets.tsx
+++ src/components/Tweets.tsx
@@ -1,9 +1,8 @@
-import { DatePicker, TimePicker } from 'antd';
+import { DatePicker, TimePicker, Input, Button } from 'antd';
 import moment, { Moment } from 'moment';
 import React from 'react';
 
 function Tweets() {
-  console.log(moment().hour(), moment().minute());
   function disabledDate(current: Moment): boolean {
     return current <= moment().subtract(1, 'day');
   }
@@ -22,38 +21,44 @@ function Tweets() {
       for (let i = 0; i <= moment().minute(); i++) {
         minutes.push(i);
       }
-    } else {
-      for (let i = 0; i < 60; i++) {
-        minutes.push(i);
-      }
     }
     return minutes;
   }
 
   function handleChange(date: Moment | null, dateString: string): void {
-    console.log(date, dateString);
+    console.log(typeof date, date, typeof dateString, dateString);
   }
 
   function handleTimeChange(time: Moment | null, timeString: string): void {
-    console.log(time, timeString);
+    console.log(typeof time, time, typeof timeString, timeString);
   }
 
   return (
-    <div className='flex'>
-      <DatePicker
-        size={'large'}
-        disabledDate={disabledDate}
-        onChange={handleChange}
-      />
-      <TimePicker
-        size={'large'}
-        format='HH:mm'
-        onChange={handleTimeChange}
-        disabledHours={disabledHours}
-        disabledMinutes={disabledMinutes}
-        hideDisabledOptions
-      />
-    </div>
+    <>
+      <div style={{ width: '50%' }}>
+        <Input.TextArea
+          placeholder='Type in your tweet here'
+          autoSize={{ minRows: 3, maxRows: 8 }}
+          className='input-area'
+        />
+        <Button type='primary'>Tweet</Button>
+      </div>
+      <div className='flex'>
+        <DatePicker
+          size={'large'}
+          disabledDate={disabledDate}
+          onChange={handleChange}
+        />
+        <TimePicker
+          size={'large'}
+          format='HH:mm'
+          onChange={handleTimeChange}
+          disabledHours={disabledHours}
+          disabledMinutes={disabledMinutes}
+          hideDisabledOptions
+        />
+      </div>
+    </>
   );
 }
 
